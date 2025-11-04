b4c3bcbe6517077882faac56c387c2fed5929e7d
AdamAlmadi98 <adam.firas49@gmail.com>
2025-11-04 20:50:02 +0100

    Løs merge-konflikter, legg til SearchSummary og rydd headers

--- Search.h
+++ Search.h
@@ -1,21 +1,12 @@
-#ifndef SEARCH_H
-#define SEARCH_H
-
+#pragma once
 #include <vector>
-#include <string>
+#include "TBankAccount.h"
 #include "SearchSummary.h"
-#include "TBankAccount.h" // bytt til "TBankAccount.h" hvis det er filnavnet ditt
 
-using FCompareAccount = int(*)(const TBankAccount&, const TBankAccount&);
+// Funksjonspeker for sammenligning: returner <0, 0, >0
+using FCompareAccount = int (*)(const TBankAccount&, const TBankAccount&);
 
-int CompareByAccountNumber(const TBankAccount& a, const TBankAccount& b);
-int CompareByFullName(const TBankAccount& a, const TBankAccount& b);
-int CompareByBalance(const TBankAccount& a, const TBankAccount& b);
-
-TBankAccount MakeKeyByAccountNumber(const std::string& acc);
-TBankAccount MakeKeyByFullName(const std::string& first, const std::string& last);
-TBankAccount MakeKeyByBalance(double balance);
-
+// Sorter og søk
 void SortAccounts(std::vector<TBankAccount>& arr, FCompareAccount cmp);
 
 SearchSummary LinearSearch(const std::vector<TBankAccount>& arr,
@@ -26,4 +17,6 @@ SearchSummary BinarySearch(const std::vector<TBankAccount>& arr,
                            const TBankAccount& key,
                            FCompareAccount cmp);
 
-#endif
+// Sammenlignere som main.cpp sannsynligvis bruker
+int CompareByFullName(const TBankAccount& a, const TBankAccount& b);
+int CompareByAccountNumber(const TBankAccount& a, const TBankAccount& b);
--- SearchSummary.h
+++ SearchSummary.h
@@ -1,37 +1,8 @@
-<<<<<<< HEAD
-//
-// Created by adamf on 29.10.2025.
-//
-
-#ifndef SIMPLESEARCH_SEARCHSUMMARY_H
-#define SIMPLESEARCH_SEARCHSUMMARY_H
-
-
-
-struct SearchSummery{
-    long long comparisons;
-    double timeSpentsMs;
-
-SearchSummery()
-    : comparisons(0),
-      timeSpentMs(0.0)
- {}
-};
-
-
-#endif //SIMPLESEARCH_SEARCHSUMMARY_H
-=======
-#ifndef SEARCH_SUMMARY_H
-#define SEARCH_SUMMARY_H
+#pragma once
 
 struct SearchSummary {
-    int comparisons = 0;
-    int steps = 0;
-    int foundIndex = -1;
-    double timeSpentMs = 0.0;
-
-    bool found() const { return foundIndex >= 0; }
+    int    foundIndex = -1;   // -1 hvis ikke funnet
+    int    steps      = 0;    // teller iterasjoner
+    int    comparisons = 0;   // valgfritt, brukes om du vil logge sammenligninger
+    double timeSpentMs = 0.0; // tidsbruk i millisekunder
 };
-
-#endif
->>>>>>> 23d6da3 (Initial commit of SimpleSearch project)
